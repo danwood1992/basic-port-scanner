b7d811814b4aff36a4c43fef1950b2736b52fc7d
daniel wood <gunnerwoody449@gmail.com>
2022-08-07 17:56:42 +0100

    modified:   cybrascan.py

--- cybrascan.py
+++ cybrascan.py
@@ -1,15 +1,17 @@
+#!/usr/bin/python3
+
 import nmap
 
-nm = nmap.PortScanner()
+scanner = nmap.PortScanner()
 
-print("cybrascan  v1.0 ")
+print("Welcome, this is a simple nmap automation tool")
 print("<----------------------------------------------------->")
 
-ipv4 = input("Enter IPv4 address : ")
-print("IP you entered is: ", ipv4)
-type(ipv4)
+ip_addr = input("Please enter the IP address you want to scan: ")
+print("The IP you entered is: ", ip_addr)
+type(ip_addr)
 
-resp = input("""\nScan you want to run
+resp = input("""\nPlease enter the type of scan you want to run
                 1)SYN ACK Scan
                 2)UDP Scan
                 3)Comprehensive Scan \n""")
@@ -18,9 +20,10 @@ resp_dict={'1':['-v -sS','tcp'],'2':['-v -sU','udp'],'3':['-v -sS -sV -sC -A -O'
 if resp not in resp_dict.keys():
     print("enter a valid option")
 else:
-        nm.scan(ipv4,"1-1024",resp_dict[resp][0]) #the # are port range to scan, the last part is the scan type
-    print(nm.scaninfo())
-    if nm.scaninfo()=='up':
-        print("Scanner Status: ", nm[ipv4].state())
-        print(nm[ipv4].all_protocols())
-        print("Open Ports: ",nm[ipv4][resp_dict[resp][1]].keys())  #display all open ports
+    print("nmap version: ", scanner.nmap_version())
+    scanner.scan(ip_addr,"1-1024",resp_dict[resp][0]) #the # are port range to scan, the last part is the scan type
+    print(scanner.scaninfo())
+    if scanner.scaninfo()=='up':
+        print("Scanner Status: ",scanner[ip_addr].state())
+        print(scanner[ip_addr].all_protocols())
+        print("Open Ports: ",scanner[ip_addr][resp_dict[resp][1]].keys())  #display all open ports
