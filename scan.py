5c9b7a0b00afb7aeef6a850c73afa34f4f7c74ba
Daniel Wood <109208162+danwood1992@users.noreply.github.com>
2023-05-03 14:53:14 +0100

    Update scan.py

--- scan.py
+++ scan.py
@@ -2,7 +2,7 @@
 import nmap
 import pyfiglet
 
-banner = pyfiglet.figlet_format("cybrascan", justify ='centre', font='cybermedium')
+banner = pyfiglet.figlet_format("Network Scanner", justify ='centre', font='cybermedium')
 
 scanner = nmap.PortScanner()
 print(banner)
@@ -17,14 +17,22 @@ resp = input("""\nPlease enter the scan you want to run
                 1)SYN ACK Scan (TCP)
                 2)UDP Scan
                 3)Comprehensive Scan \n""")
+                
 print("You have selected option: ", resp)
+
 resp_dict={'1':['-v -sS','tcp'],'2':['-v -sU','udp'],'3':['-v -sS -sV -sC -A -O','tcp']}
+
 if resp not in resp_dict.keys():
+
     print("enter a valid option")
+    
 else:
     print("nmap version: ", scanner.nmap_version())
+    
     scanner.scan(ipv4_address,"1-1024",resp_dict[resp][0]) #the port range to scan, the last part is the scan type
+    
     print(scanner.scaninfo())
+    
     if scanner.scaninfo()=='up':
         print("Scanner Status: ",scanner[ipv4_address].state())
         print(scanner[ipv4_address].all_protocols())
